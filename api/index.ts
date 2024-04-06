f47ebc7b0a38e381743a2ae486dd375364f2ddcf
Serrano Horacio <serranohoraciohector@gmail.com>
2024-04-06 20:19:41 -0300

    .

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Proyecto Para Practica Profesional Supervisada UTN 2024"));
+app.get("/", (req, res) => res.send("<p>Proyecto Para Practica Profesional Supervisada UTN 2024</p> <p>Para ejecutar local: vercel dev</p>"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
