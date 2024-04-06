72021cd1f2e07649dc0d1de12be5e99677c44e58
Serrano Horacio <serranohoraciohector@gmail.com>
2024-04-06 20:15:56 -0300

    Cambia landing Inicial

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Proyecto Para Practica Profesional Supervisada UTN 2024"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
