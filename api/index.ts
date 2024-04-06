eeefdbf357a8b595c577b85fb021dfc25f2b64a6
Serrano Horacio <serranohoraciohector@gmail.com>
2024-04-06 20:03:40 -0300

    Añade Tipos para Require

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,6 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(3001, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
