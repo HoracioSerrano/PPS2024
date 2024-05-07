806a0ac770fc7595240c7d601fffd4f1e235bfff
Serrano Horacio <serranohoraciohector@gmail.com>
2024-05-06 23:11:57 -0300

    prueba

--- api/index.ts
+++ api/index.ts
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.post('/api/login', function (req:any, res:any, next:any) {
     console.log('middleware');
     console.log(req.body)
-    //res.json(req.body)
+    res.json(req.body)
   })
 
 app.post('/api/login', function (req:any, res:any) {
